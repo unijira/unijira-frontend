ef46c8dfaf0a03ef4f7d5bfce54e06b92d3b010c
danielesalimonti <danielesalim@outlook.it>
2021-12-05 14:49:38 +0100

    Added i18n (internationalization language) and AuthGuard for login

---
+++ unijira/src/app/classes/auth-guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {SessionService} from "../store/session.service";
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private sessionService: SessionService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    let isLogged = false;
+    const sub = this.sessionService.getIsUserLogged().subscribe(log => isLogged = log);
+    sub.unsubscribe();
+    if (isLogged) {
+      // logged in so return true
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
