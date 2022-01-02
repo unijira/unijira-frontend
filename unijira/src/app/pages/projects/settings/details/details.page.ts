1ae7db2f06238e4648a806aa6d4144e551256920
Perfidio Matteo <perfidiomatteo7@gmail.com>
2022-01-02 13:57:23 +0100

    Add settings about details of the project

--- unijira/src/app/pages/projects/settings/details/details.page.ts
+++ unijira/src/app/pages/projects/settings/details/details.page.ts
@@ -4,8 +4,10 @@ import {Subscription} from 'rxjs';
 import {SessionService} from '../../../../store/session.service';
 import {ProjectService} from '../../../../services/common/project.service';
 import {FormControl, Validators} from '@angular/forms';
-import {AlertController} from '@ionic/angular';
-import {ActivatedRoute} from '@angular/router';
+import {AlertController, ToastController} from '@ionic/angular';
+import {ActivatedRoute, Router} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
+import {FileUploadService} from '../../../../services/common/file-upload.service';
 
 @Component({
   selector: 'app-details',
@@ -26,15 +28,22 @@ export class DetailsPage implements OnInit {
   constructor(private sessionService: SessionService,
               public alertController: AlertController,
               private projectService: ProjectService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private translateService: TranslateService,
+              private uploadService: FileUploadService,
+              private router: Router,
+              public toastController: ToastController) {
 
     this.projectSubscription = this.sessionService.getProject().subscribe((p) => {
       this.project = p;
+      this.nameForm.setValue(this.project && this.project.name);
+      this.keyForm.setValue(this.project && this.project.key);
     });
 
     this.activatedRoute.params.subscribe(params => this.sessionService.loadProject(params.id));
 
   }
+
   ngOnInit() {
     this.image = '';
     this.nameForm.setValue(this.project && this.project.name);
@@ -75,10 +84,130 @@ export class DetailsPage implements OnInit {
 
   save() {
 
+    this.showAlert(this.translateService.instant('project.settings.details.alert.update.title'),
+      this.translateService.instant('project.settings.details.alert.update.message.sure'),
+      this.translateService.instant('wizard.alert.message.button.cancel'),
+      this.translateService.instant('wizard.alert.message.button.confirm'))
+      .then(res => {
+
+        if (res) {
+
+          if(this.file !== undefined) {
+
+            this.uploadService.upload(this.project.id, 'icon', this.file).subscribe(
+              url => {
+                this.projectService.updateProject(this.project.id, this.nameForm.value, this.keyForm.value,
+                  this.project.ownerId, new URL(url)).subscribe(
+                  p => {
+
+                    if(p !== null) {
+                      this.presentToast(this.translateService.instant('project.settings.details.toast.update.success')).then();
+                    } else {
+                      this.presentToast(this.translateService.instant('project.settings.details.toast.update.failed')).then();
+                    }
+
+                  }
+                );
+              }
+            );
+
+          } else {
+
+            this.projectService.updateProject(this.project.id, this.nameForm.value, this.keyForm.value,
+              this.project.ownerId, this.project.icon).subscribe(
+              p => {
+
+                if(p !== null) {
+                  this.presentToast(this.translateService.instant('project.settings.details.toast.update.success')).then();
+                } else {
+                  this.presentToast(this.translateService.instant('project.settings.details.toast.update.failed')).then();
+                }
+
+              }
+            );
+
+          }
+
+        }
+
+      });
+
   }
 
   delete() {
 
+    this.showAlert(this.translateService.instant('project.settings.details.alert.delete.title'),
+      this.translateService.instant('project.settings.details.alert.delete.message.sure'),
+      this.translateService.instant('wizard.alert.message.button.cancel'),
+      this.translateService.instant('wizard.alert.message.button.confirm'))
+      .then(res => {
+
+        if (res) {
+
+          this.projectService.deleteProject(this.project.id).subscribe(
+            p => {
+
+              if(p === null) {
+
+                this.router.navigate(['home']).then(
+                  e => this.presentToast(this.translateService.instant('project.settings.details.toast.delete.success')).then()
+                );
+
+              } else {
+                this.presentToast(this.translateService.instant('project.settings.details.toast.delete.failed')).then();
+              }
+
+            }
+          );
+
+        }
+
+      });
+
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      position: 'top',
+      duration: 4000
+    });
+    await toast.present();
+  }
+
+  async showAlert(header: string, message: string, cancel: string, confirm: string): Promise<any> {
+
+    return new Promise(async (resolve) => {
+
+      const alert = await this.alertController.create({
+        header,
+        message,
+        buttons: [
+          {
+            text: cancel,
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              resolve(false);
+            }
+          }, {
+            text: confirm,
+            handler: () => {
+              resolve(true);
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+
+    });
+  }
+
+  verifyChanges() {
+    return this.nameForm.value === this.project.name &&
+           this.keyForm.value  === this.project.key  &&
+           this.file           === undefined;
   }
 
 }
