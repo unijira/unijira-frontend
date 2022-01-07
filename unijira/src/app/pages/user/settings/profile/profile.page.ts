9b94de3609fd665f51fe2303ff02aac57353fd82
pietrocfn02 <pietrocf02@outlook.it>
2022-01-07 12:39:44 +0100

    Fixed some stuff

--- unijira/src/app/pages/user/settings/profile/profile.page.ts
+++ unijira/src/app/pages/user/settings/profile/profile.page.ts
@@ -11,19 +11,6 @@ import {Project} from '../../../../models/projects/Project';
 })
 export class ProfilePage implements OnInit {
   @Input() user: UserInfo;
-  @Input() firstName: string;
-  @Input() lastName: string;
-  @Input() username: string;
-  @Input() avatar: URL;
-  @Input() createdAt: Date;
-  @Input() updatedAt: Date;
-  @Input() role: string;
-  @Input() description: string;
-  @Input() github: string;
-  @Input() linkedin: string;
-  @Input() phoneNumber: string;
-  @Input() birthdate: string;
-
   enrolledProjects: Project[] = [];
   collaborators: UserInfo[] = [];
   constructor(
@@ -42,56 +29,14 @@ export class ProfilePage implements OnInit {
     this.collaborators.push(collab,collab,collab,collab,collab,collab,collab);
 
   }
-  copyAll(userinfo: UserInfo) {
-    this.user = userinfo;
-    if (!this.user.firstName === null) {
-      this.firstName = this.user.firstName;
-    }
-    if (!this.user.username === null) {
-      this.username = this.user.username;
-    }
-    if (!this.user.lastName === null) {
-      this.lastName = this.user.lastName;
-    }
-    if (!this.user.avatar === null) {
-      this.avatar = this.user.avatar;
-    }
-    if (!this.user.role === null) {
-      this.role = this.user.role;
-    }
-    if (!this.user.github === null) {
-      this.github = this.user.github;
-    }
-    if (!this.user.description === null) {
-      this.description = this.user.description;
-    }
-    if (!this.user.createdAt === null) {
-      this.createdAt = this.user.createdAt;
-    }
-    if (!this.user.updatedAt === null) {
-      this.updatedAt = this.user.updatedAt;
-    }
-    if (!this.user.linkedin === null) {
-      this.linkedin = this.user.linkedin;
-    }
-    if (!this.user.phoneNumber === null) {
-      this.phoneNumber = this.user.phoneNumber;
-    }
-  }
   ngOnInit() {
-    this.accountService.me().subscribe(value => this.copyAll(value));
+    this.user = new UserInfo(null,null,null,null,null,null,
+      null,null,null,null,null,null,null, null,
+      null);
+    this.accountService.me().subscribe(value => {this.user = value;});
     this.sampleInit();
   }
   copyBack() {
-    this.user.firstName = this.firstName;
-    this.user.lastName = this.lastName;
-    this.user.username = this.username;
-    this.user.role = this.role;
-    this.user.avatar = this.avatar;
-    this.user.description = this.description;
-    this.user.linkedin = this.linkedin;
-    this.user.github = this.github;
-    this.user.phoneNumber = this.phoneNumber;
     this.dummy();
   }// Just print object
   dummy() {
